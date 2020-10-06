0c9c8026a5297361e88fc91c0734cf56419e46dd
Alan Birouska <birouska@gmail.com>
2020-10-06 10:03:57 -0300

    feat: add convert-images

--- robots/image.js
+++ robots/image.js
@@ -1,4 +1,7 @@
 const imageDownloader = require('image-downloader')
+const gm = require('gm').subClass({
+    imageMagick: true
+})
 const google = require('googleapis').google
 const customSearch = google.customsearch('v1')
 const state = require('./state.js')
@@ -8,17 +11,20 @@ const googleSearchCredentials = require('../credentials/google-search.json')
 async function robots() {
     const content = state.load()
 
-    await fetchImagesOfAllSentences(content)
-    await downloadAllImages(content)
+    //await fetchImagesOfAllSentences(content)
+    //await downloadAllImages(content)
+    //await convertAllImages(content)
+    //await createAllSentencesImages(content)
+    await createYouTubeThumbnail()
 
-    state.save(content)
+    //state.save(content)
 
     async function fetchImagesOfAllSentences(content) {
-        for(const sentence of content.sentences) {
+        for (const sentence of content.sentences) {
             const query = `${content.searchTerm} ${sentence.keywords[0]} ${sentence.keywords[1]}`
             sentence.images = await fetchGoogleAndReturnImagesLinks(query)
 
-            sentence.googleSearchQuery =  query
+            sentence.googleSearchQuery = query
         }
     }
 
@@ -37,18 +43,18 @@ async function robots() {
 
         return imagesUrl
     }
-    
+
     async function downloadAllImages(content) {
         content.downloadedImages = []
 
-        for(let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
+        for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
             const images = content.sentences[sentenceIndex].images
 
-            for(let imageIndex = 0; imageIndex < images.length; imageIndex++) {
+            for (let imageIndex = 0; imageIndex < images.length; imageIndex++) {
                 const imageUrl = images[imageIndex]
 
                 try {
-                    if(content.downloadedImages.includes(imageUrl)) {
+                    if (content.downloadedImages.includes(imageUrl)) {
                         throw new Error('Imagem já baixada')
                     }
 
@@ -65,11 +71,132 @@ async function robots() {
 
     async function downloadAndSave(url, filename) {
         return imageDownloader.image({
-            url, url, 
+            url,
+            url,
             dest: `./content/${filename}`
         })
     }
+
+    async function convertAllImages(content) {
+        for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
+            await convertImage(sentenceIndex)
+        }
+    }
+
+    async function convertImage(sentenceIndex) {
+        return new Promise((resolve, reject) => {
+            const inputFile = `./content/${sentenceIndex}-original.png[0]`
+            const outputFile = `./content/${sentenceIndex}-converted.png`
+            const width = 1920
+            const height = 1080
+
+            gm()
+                .in(inputFile)
+                .out('(')
+                .out('-clone')
+                .out('0')
+                .out('-background', 'white')
+                .out('-blur', '0x9')
+                .out('-resize', `${width}x${height}^`)
+                .out(')')
+                .out('(')
+                .out('-clone')
+                .out('0')
+                .out('-background', 'white')
+                .out('-resize', `${width}x${height}`)
+                .out(')')
+                .out('-delete', '0')
+                .out('-gravity', 'center')
+                .out('-compose', 'over')
+                .out('-composite')
+                .out('-extent', `${width}x${height}`)
+                .write(outputFile, (error) => {
+                    if (error) {
+                        return reject(error)
+                    }
+
+                    console.log(`> [video-robot] Image converted: ${outputFile}`)
+                    resolve()
+                })
+
+        })
+    }
+
+    async function createAllSentencesImages(content) {
+        for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
+            await createSentenceImage(sentenceIndex, content.sentences[sentenceIndex].text)
+        }
+    }
+
+    async function createSentenceImage(sentenceIndex, sentenceText) {
+        return new Promise((resolve, reject) => {
+            const outputFile = `./content/${sentenceIndex}-sentence.png`
+            const templateSettings = {
+                0: {
+                    size: '1920x400',
+                    gravity: 'center'
+                },
+                1: {
+                    size: '1920x1080',
+                    gravity: 'center'
+                },
+                2: {
+                    size: '800x1080',
+                    gravity: 'west'
+                },
+                3: {
+                    size: '1920x400',
+                    gravity: 'center'
+                },
+                4: {
+                    size: '1920x1080',
+                    gravity: 'center'
+                },
+                5: {
+                    size: '800x1080',
+                    gravity: 'west'
+                },
+                6: {
+                    size: '1920x400',
+                    gravity: 'center'
+                }
+
+            }
+
+            gm()
+                .out('-size', templateSettings[sentenceIndex].size)
+                .out('-gravity', templateSettings[sentenceIndex].gravity)
+                .out('-background', 'transparent')
+                .out('-fill', 'white')
+                .out('-kerning', '-1')
+                .out(`caption:${sentenceText}`)
+                .write(outputFile, (error) => {
+                    if (error) {
+                        return reject(error)
+                    }
+
+                    console.log(`> [video-robot] Sentence created: ${outputFile}`)
+                    resolve()
+                })
+        })
+
+    }
+
+    async function createYouTubeThumbnail() {
+        return new Promise((resolve, reject) => {
+          gm()
+            .in('./content/0-converted.png')
+            .write('./content/youtube-thumbnail.jpg', (error) => {
+              if (error) {
+                return reject(error)
+              }
     
+              console.log('> [video-robot] YouTube thumbnail created')
+              resolve()
+            })
+        })
+      }
 }
+
 
 module.exports = robots
